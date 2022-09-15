5a375e6ddbe988adcd86a35d8ea3c5246e6a82af
chengweiZhang <zcw9820@163.com>
2022-09-15 20:47:11 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zhangchengwei
+ * @create 2022-09-15 20:33
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello.git!");
+    }
+}
